fef700c9f9a1df7e59f1a6de73a125165cd0dd4d
Shubhanshu <shubhanshuchoudhary2006@gmail.com>
2026-07-31 20:40:51 +0530

    Remove mobile view from DissolveHeroSection
    Removed mobile view section for the DissolveHeroSection component.

--- src/components/sections/DissolveHeroSection.tsx
+++ src/components/sections/DissolveHeroSection.tsx
@@ -9,25 +9,12 @@ const ScrollDissolveReveal = dynamic(
 export default function DissolveHeroSection() {
   return (
     <section className="w-full bg-nx-bg">
-      {/* Mobile View */}
-      <div className="block md:hidden">
-        <ScrollDissolveReveal
-          imageFront="/images/BG.png" // Mobile Front Image
-          imageBack="/images/LOGO NEXURA 1.png"
-          containerClassName="w-full"
-          className="w-full"
-        />
-      </div>
-
-      {/* Laptop / Desktop View */}
-      <div className="hidden md:block">
         <ScrollDissolveReveal
           imageFront="/images/BG.png" // Laptop/Desktop Front Image
           imageBack="/images/LOGO NEXURA 1.png"
           containerClassName="w-full"
           className="w-full"
         />
-      </div>
     </section>
   )
 }
